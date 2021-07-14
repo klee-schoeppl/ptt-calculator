8d22117417d56c602e3c3c464302c8dd5b0458e4
Leon Schöppl <37944147+Eondo@users.noreply.github.com>
2021-07-14 15:44:50 +0200

    Update main.R
    Most current version from pre-git version control. Includes a fully functional delta-P subroutine. Includes an incomplete interpretations subroutine.

--- main.R
+++ main.R
@@ -263,7 +263,7 @@ A<-2 + a1  # the total number of A
 C<-1 + c1 # the total number of C
 nA<- 3 + (1-a1) # the total number of not-A
 AC<-1 + a1*c1 # the total number of A and C
-CnA<- 1 + c1*(1-a1)# the total number of not-A and C
+CnA<- 0 + c1*(1-a1)# the total number of not-A and C
 pCgA<-AC/A
 pCgnA<-CnA/nA
 deltaP<-pCgA-pCgnA
@@ -625,11 +625,10 @@ xtable(output,type="latex")
 
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
-# TRYING MY HANDS AT A SUBROUTINE FOR THIS PROBLEM
-
+# TRYING MY HANDS AT A SUBROUTINE FOR DELTA-P
 calculateDeltaP <- function(vecA, vecC) {
   #Expects as input 2 equal length vectors containing 6 or fewer boolean values 
-  #each. VecA should contain whether the die in question makes the 
+  #each. VecA should contain whether the side in question makes the 
   #antecedent of the conditional true, VecC the conditional. 
   
   if (!is.vector(vecA) |!is.vector(vecC)){
@@ -644,81 +643,49 @@ calculateDeltaP <- function(vecA, vecC) {
   if (!all(is.logical(vecC))){
     stop('Invalid input: Vector C contains non-boolean value.')
   }
+  if (length(vecA)>6){
+    stop('Invalid input: Vectors contain too many values.')
+  }
   
   #-----------------------------------------------------------------------------
   #Vectors shorter than 6 values are taken to be uncertainty tasks with an 
   #appropriate amount of blank sides.
-  a1 <- c(0)
-  a2 <- c(0)
-  a3 <- c(0)
-  a4 <- c(0)
-  a5 <- c(0)
-  a6 <- c(0)
-  aBlanks = list(a1, a2, a3, a4, a5, a6)
-  c1 <- c(0)
-  c2 <- c(0)
-  c3 <- c(0)
-  c4 <- c(0)
-  c5 <- c(0)
-  c6 <- c(0)
-  cBlanks = list(c1, c2, c3, c4, c5, c6)
-  #rm(constituents)
-  consituents <- list()
-  print(length(vecA))
+  aBlanks = list(c(0), c(0), c(0), c(0), c(0), c(0))
+  cBlanks = list(c(0), c(0), c(0), c(0), c(0), c(0))#eventuell NULL setzen
+  consituents <- vector(mode = "list", length = 0)
+  constituents <<- NULL
   numberOfConceiledSides <- (6 - length(vecA))
   
-  if (numberOfConceiledSides>0){ #preparation to expand the grid
-    for (i in 1 :numberOfConceiledSides ){
+  if (numberOfConceiledSides > 0){ 
+    for (i in 1 :numberOfConceiledSides ){ #preparation to expand the grid
       additionalVariable <- list(c(0,1),c(0,1))
       constituents <- c(constituents, additionalVariable)
     }
-  }
-  print('test')
-  print(constituents)
-  print(length(constituents))
-  if(numberOfConceiledSides>0){ #gridExpansion
-    constituents <- rev(expand.grid(constituents))
-  }
-  
-  if (numberOfConceiledSides>0){
-    for (i in 1 : numberOfConceiledSides){
-      
+    constituents <- rev(expand.grid(constituents)) #grid expansion
+    for (i in 1 : numberOfConceiledSides){ #grid to vectors
       aBlanks[[i]]<-constituents[,i]
       cBlanks[[i]]<-constituents[,(i+numberOfConceiledSides)]
-    }}
-  
-  print(aBlanks)
-  
+    }
+  }
   #-----------------------------------------------------------------------------
   #Reads out the inputs and (if applicable) the arrays containing the variables
   #for blank sides. 
-  A <- sum(vecA) #counts all T entries in the vector
-  A <- A + sum(aBlanks[[1]],aBlanks[[2]],aBlanks[[3]],aBlanks[[4]],aBlanks[[5]],aBlanks[[6]]) #irrelevant values equal 0
-  print(A)
-  C <- sum(vecC) #counts all T entries in the vector
-  C <- C + sum(cBlanks[[1]],cBlanks[[2]],cBlanks[[3]],cBlanks[[4]],cBlanks[[5]],cBlanks[[6]]) #irrelevant values equal 0
-  print(C)
+  A <- (sum(vecA) + aBlanks[[1]] + aBlanks[[2]] + aBlanks[[3]] + aBlanks[[4]] 
+        + aBlanks[[5]] + aBlanks[[6]]) #irrelevant values equal 0
+  C <- (sum(vecC) + cBlanks[[1]] + cBlanks[[2]] + cBlanks[[3]] + cBlanks[[4]] 
+        + cBlanks[[5]] + cBlanks[[6]]) #irrelevant values equal 0
   nA<- 6 - A
-  print(nA)
-  AC<-0 + aBlanks[[1]]*cBlanks[[1]] + aBlanks[[2]]*cBlanks[[2]] 
-  + aBlanks[[3]]*cBlanks[[3]] + aBlanks[[4]]*cBlanks[[4]] + aBlanks[[5]]*cBlanks[[5]] 
-  + aBlanks[[6]]*cBlanks[[6]] 
+  tempAC <- 0
   for (i in 1 : length(vecA)){
-    if (vecA[i] & vecC[i]) {
-      AC <- AC + 1
+    if (vecA[i] & vecC[i]){
+      tempAC <- tempAC + 1
     }
   }
-  print(AC)
-  CnA<- 0 + cBlanks[[1]]*(1-aBlanks[[1]]) + cBlanks[[2]]*(1-aBlanks[[2]]) 
-  + cBlanks[[3]]*(1-aBlanks[[3]]) + cBlanks[[4]]*(1-aBlanks[[4]]) 
-  + cBlanks[[5]]*(1-aBlanks[[5]])+ cBlanks[[6]]*(1-aBlanks[[6]])
-  for (i in 1 : length(vecA)){
-    if (!vecA[i] & vecC[i]){
-      CnA <- CnA + 1
-    }
-  }
-  print(CnA)
+  AC<- (tempAC + aBlanks[[1]]*cBlanks[[1]] + aBlanks[[2]]*cBlanks[[2]]
+        + aBlanks[[3]]*cBlanks[[3]] + aBlanks[[4]]*cBlanks[[4]] 
+        + aBlanks[[5]]*cBlanks[[5]]  + aBlanks[[6]]*cBlanks[[6]])
   
+  CnA <- C - AC
   #-----------------------------------------------------------------------------
   #Calculates delta P, substituting 0 for undefined values resulting from 
   #division by 0
@@ -728,28 +695,123 @@ calculateDeltaP <- function(vecA, vecC) {
       pCgA[i]<-c(0)
     }
   }
-  print(pCgA)
   pCgnA<-CnA/nA
   for (i in 1 : length(pCgnA)){
     if (is.na(pCgnA[i])){
       pCgnA[i]<-c(0)
     }
   }
-  print(pCgnA)
   deltaP<-pCgA-pCgnA
-  
+  print(length(deltaP))
   return(summary(cbind(deltaP))) #returns the summary of the delta P column
 }
+
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
-# TESTING GROUND FOR MY SUBROUTINE
-vectorA <- c(T,F,F,F,F)
-vectorC <- c(T,T,T,F,F)
+# TRYING MY HANDS AT A SUBROUTINE FOR THE INTERPRETATIONS
+
+calculateInterpretations <- function(vecA, vecC) {
+  #Expects as input 2 equal length vectors containing 6 or fewer boolean values 
+  #each. VecA should contain whether the side in question makes the 
+  #antecedent of the conditional true, VecC the conditional. 
+  
+  if (!is.vector(vecA) |!is.vector(vecC)){
+    stop('Invalid input: Excpected 2 vectors')
+  }
+  if (length(vecA)!= length(vecC)){
+    stop('Invalid input: Unequal vector length.')
+  }
+  if (!all(is.logical(vecA))) {
+    stop('Invalid input: Vector A contains non-boolean value.')
+  }
+  if (!all(is.logical(vecC))){
+    stop('Invalid input: Vector C contains non-boolean value.')
+  }
+  if (length(vecA)>6){
+    stop('Invalid input: Vectors contain too many values.')
+  }
+  
+  #-----------------------------------------------------------------------------
+  #Vectors shorter than 6 values are taken to be uncertainty tasks with an 
+  #appropriate amount of blank sides.
+  aBlanks = list(c(0), c(0), c(0), c(0), c(0), c(0))
+  cBlanks = list(c(0), c(0), c(0), c(0), c(0), c(0))#eventuell NULL setzen
+  consituents <- vector(mode = "list", length = 0)
+  constituents <<- NULL
+  numberOfConceiledSides <- (6 - length(vecA))
+  
+  if (numberOfConceiledSides > 0){ 
+    for (i in 1 :numberOfConceiledSides ){ #preparation to expand the grid
+      additionalVariable <- list(c(0,1),c(0,1))
+      constituents <- c(constituents, additionalVariable)
+    }
+    constituents <- rev(expand.grid(constituents)) #grid expansion
+    for (i in 1 : numberOfConceiledSides){ #grid to vectors
+      aBlanks[[i]]<-constituents[,i]
+      cBlanks[[i]]<-constituents[,(i+numberOfConceiledSides)]
+    }
+  }
+  #-----------------------------------------------------------------------------
+  #Reads out the inputs and (if applicable) the arrays containing the variables
+  #for blank sides. 
+  A <- (sum(vecA) + aBlanks[[1]] + aBlanks[[2]] + aBlanks[[3]] + aBlanks[[4]] 
+        + aBlanks[[5]] + aBlanks[[6]]) #irrelevant values equal 0
+  C <- (sum(vecC) + cBlanks[[1]] + cBlanks[[2]] + cBlanks[[3]] + cBlanks[[4]] 
+        + cBlanks[[5]] + cBlanks[[6]]) #irrelevant values equal 0
+  nA<- 6 - A
+  nC<- 6 - C
+  tempAC <- 0
+  for (i in 1 : length(vecA)){
+    if (vecA[i] & vecC[i]){
+      tempAC <- tempAC + 1
+    }
+  }
+  AC<- (tempAC + aBlanks[[1]]*cBlanks[[1]] + (1-aBlanks[[2]])*(1-cBlanks[[2]])
+        + (1-aBlanks[[3]])*(1 - cBlanks[[3]]) + (1-aBlanks[[4]])*(1-cBlanks[[4]]) 
+        + (1-aBlanks[[5]])*(1-cBlanks[[5]])  + (1-aBlanks[[6]])*(1-cBlanks[[6]]))
+  tempnAnC <- 0
+  for (i in 1 : length(vecA)){
+    if (!vecA[i] & !vecC[i]){
+      tempnAnC <- tempnAnC + 1
+    }
+  }
+  nAnC <- (tempnAnC + (1-aBlanks[[1]])*(1-cBlanks[[1]]) + aBlanks[[2]]*cBlanks[[2]]
+           + aBlanks[[3]]*cBlanks[[3]] + aBlanks[[4]]*cBlanks[[4]] 
+           + aBlanks[[5]]*cBlanks[[5]]  + aBlanks[[6]]*cBlanks[[6]])
+  
+  CnA <- C - AC
+  AnC <- A - AC
+  #-----------------------------------------------------------------------------
+  #Calculates delta P, substituting 0 for undefined values resulting from 
+  #division by 0
+  materialConditional <- ((AC + CnA + nAnC)/6) #continue later
+  print(summary(cbind(materialConditional)))
+  equivalent <- ((AC + nAnC)/6)
+  print(summary(cbind(equivalent)))
+  conjunction <- (AC/6)
+  print(summary(cbind(conjunction)))
+  
+  pCgA<-AC/A
+  for (i in 1 : length(pCgA)){
+    if (is.na(pCgA[i])){
+      pCgA[i]<-c(0)
+    }
+  }
+  pCgnA<-CnA/nA
+  for (i in 1 : length(pCgnA)){
+    if (is.na(pCgnA[i])){
+      pCgnA[i]<-c(0)
+    }
+  }
+  deltaP<-pCgA-pCgnA
+  
+  print(length(deltaP))
+  return(summary(cbind(deltaP))) #returns the summary of the delta P column
+}
+
+vectorA <- c(F,F,F,F)
+vectorC <- c(F,F,F,F)
 calculateDeltaP(vectorA, vectorC)
-
-
-
-
-
+calculateInterpretations(vectorA, vectorC)
 
 
