a9925bb10bd551375748a085691a285b45b28112
Leon Schöppl <37944147+leon-schoeppl@users.noreply.github.com>
2021-09-10 12:39:33 +0200

    Notions now optional and connective type matters;

--- PTT-Calculator/ui.R
+++ PTT-Calculator/ui.R
@@ -10,13 +10,23 @@ fluidPage(
     sidebarLayout(
         
         sidebarPanel(
-            h5("Choose the setup for this task. Keep in mind that for high 
-               # of [?], the calculation might take a while (Θ(2^2n))."),
+            h4("Choose the setup for this task."),
+            
             selectInput("connectiveType", "Select a connective type.", 
-                        choices = c('If [A], then [C].', '[A] and [C].')),
+                        choices = c('If [A], then [C].', '[A] and [C].', '[A] o [C].')),
+            
+            h5("As the total number of possible interpretation is quite large, 
+               we have tried to group them according to the natural language 
+               connectives that they seem most appropriate for. If you wish 
+               to see every interpretation, choose '[A] o [C]'."),
+            
             sliderInput("caseCount", "Select the total number of cases.",
                         min = 2, max = 10,
                         value = 6),
+            
+            h5("Keep in mind that for high # of [?] (especially above 6), the 
+               calculation might take a while (Θ(2^2n))."),
+            
             
             numericInput("AC", "# of [A ∧ C] cases.", value = 0, min = 0, 
                          max = 10),
@@ -28,10 +38,16 @@ fluidPage(
                          max = 10),
             numericInput("blank", "# of [?] cases.", value = 0, min = 0, 
                          max = 10),
-            checkboxInput("outputL", "Include output in LaTeX format", value = FALSE, width = NULL),
+            
+            checkboxInput("outputL", "Include output in LaTeX format", 
+                          value = FALSE, width = NULL),
+            checkboxInput("includeH", "Include halfway interpretations",
+                          value = FALSE, width = NULL),
+            checkboxInput("includeN", "Include Notions of Inferrential Strength",
+                          value = FALSE, width = NULL),
             
             actionButton("show", "Display formulae"),
-            actionButton("do","Calculate")
+            actionButton("do","Calculate Task")
         ),
 
         # Show a plot of the generated distribution
